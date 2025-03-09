ddd22e9e8b7e673924aa924442f704743be60339
HeadlessWing <wingcue@gmail.com>
2025-03-08 20:33:20 -0800

    added Leaf node class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,12 +13,22 @@ class HTMLNode:
 
     def props_to_html(self):
         if self.props == None:
-            return None
+            return ""
         result = ""
         for key in self.props:
-            result += f'{key}="{self.props[key]} '
+            result += f' {key}="{self.props[key]}"'
         return result
     
     def __repr__(self):
-        return f"HtmlNode({self.tag}, {self.value}, {self.children}, {self.props_to_html()})"
-        
+        return f"HtmlNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag == None:
+            return self.value 
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -19,8 +19,17 @@ class TestHTMLNode(unittest.TestCase):
         node2 = HTMLNode("This is a text node", "Test" )
         self.assertNotEqual(node, node2)
     
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello World")
+        self.assertEqual(node.to_html(), "<p>Hello World</p>")
     
+    def test_leaf_to_html_None(self):
+        node = LeafNode(None, "Hello World")
+        self.assertEqual(node.to_html(), "Hello World")
 
+    def test_leaf_to_html_link(self):
+        node = LeafNode("a", "Link", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Link</a>')
 
 if __name__ == "__main__":
     unittest.main()
